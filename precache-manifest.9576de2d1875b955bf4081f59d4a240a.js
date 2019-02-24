7e0f80a534d3363e0a470c470013406a6c9da60d
Grenarded <ben.petlach@gmail.com>
2019-02-24 13:45:57 -0500

    Updates

---
+++ precache-manifest.9576de2d1875b955bf4081f59d4a240a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "35f31c7aa7cc82eaec5d5c37ef2de044",
+    "url": "/swapi-app/static/media/STARWARS.35f31c7a.woff"
+  },
+  {
+    "revision": "95cb66c1c365b8c0cd98",
+    "url": "/swapi-app/static/js/runtime~main.95cb66c1.js"
+  },
+  {
+    "revision": "7e0d3102df304824731f",
+    "url": "/swapi-app/static/js/main.7e0d3102.chunk.js"
+  },
+  {
+    "revision": "27aa7877672c4eeafd2b",
+    "url": "/swapi-app/static/js/1.27aa7877.chunk.js"
+  },
+  {
+    "revision": "7e0d3102df304824731f",
+    "url": "/swapi-app/static/css/main.0c4c4a8b.chunk.css"
+  },
+  {
+    "revision": "27aa7877672c4eeafd2b",
+    "url": "/swapi-app/static/css/1.30c5128b.chunk.css"
+  },
+  {
+    "revision": "899764e428f041bd124b41679010e765",
+    "url": "/swapi-app/index.html"
+  }
+];
